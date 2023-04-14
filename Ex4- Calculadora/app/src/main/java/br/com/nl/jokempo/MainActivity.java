6bae384ea8705fe63afb65cce3a3dd40be2e5a6d
Leo Trevisol <leotrevisol.2021@gmail.com>
2023-04-13 21:32:32 -0300

    iniciando calculadora

---
+++ Ex4- Calculadora/app/src/main/java/br/com/nl/jokempo/MainActivity.java
@@ -0,0 +1,26 @@
+package br.com.nl.jokempo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
