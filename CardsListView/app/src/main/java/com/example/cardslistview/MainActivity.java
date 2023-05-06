b53f1f495ca561733b818077ce86449b744a77b2
Leo Trevisol <leotrevisol.2021@gmail.com>
2023-05-06 00:01:02 -0300

    criando adapter pro listview

--- CardsListView/app/src/main/java/com/example/cardslistview/MainActivity.java
+++ CardsListView/app/src/main/java/com/example/cardslistview/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.cardslistview;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -12,35 +18,58 @@ public class MainActivity extends AppCompatActivity {
 
     private final String[] listaNomes ={"Naruto", "Jojos", "Haikyuu", "Fullmetal alchemist", "Yuyu Hakusho"};
 
+    private final int[] listaImagens ={R.drawable.cards_foreground, R.drawable.cards_foreground,
+            R.drawable.cards_foreground, R.drawable.cards_foreground, R.drawable.cards_foreground};
+
+    private final String[] listaDescricao ={"Naruto", "Jojos", "Haikyuu", "Fullmetal alchemist", "Yuyu Hakusho"};
+
     private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//FORMA SIMPLES
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+//                android.R.layout.simple_list_item_1, android.R.id.text1, listaNomes);
+//
+//        listView.setAdapter(adapter);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
-                android.R.layout.simple_list_item_1, android.R.id.text1, listaNomes);
 
         listView = findViewById(R.id.list);
+
+        ClassAdapter adapter;
+        adapter = new ClassAdapter(getApplicationContext(), R.layout.card);
+
+        for(int i = 0; i < listaNomes.length; i++){
+            DadosPersonagem dadosPersonagem;
+            dadosPersonagem = new DadosPersonagem(listaImagens[i], listaNomes[i], listaDescricao[i]);
+            adapter.add(dadosPersonagem);
+        }
+
         listView.setAdapter(adapter);
     }
 
-    class viewPersonagem{
+    static class ViewPersonagem{
         ImageView icone;
         TextView titulo;
         TextView descricao;
     }
 
-    class dadosPersonagem{
+    static class DadosPersonagem{
         int icone;
         String titulo;
         String descricao;
 
+        public DadosPersonagem(int icone, String titulo, String descricao) {
+            this.icone = icone;
+            this.titulo = titulo;
+            this.descricao = descricao;
+        }
+
         public int getIcone() {
             return icone;
         }
-
         public void setIcone(int icone) {
             this.icone = icone;
         }
@@ -59,6 +88,54 @@ public class MainActivity extends AppCompatActivity {
 
         public void setDescricao(String descricao) {
             this.descricao = descricao;
+        }
+    }
+
+    static class ClassAdapter extends ArrayAdapter{
+        public ClassAdapter(@NonNull Context context, int resource) {
+            super(context, resource);
+        }
+
+        @Override
+        public void add(@Nullable Object object) {
+            super.add(object);
+        }
+
+        @Override
+        public int getCount() {
+            return super.getCount();
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+            View view;
+            view = convertView;
+            ViewPersonagem viewPersonsagem;
+
+            if(convertView == null){
+                LayoutInflater inflater = (LayoutInflater) this.getContext().
+                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.card,parent,false);
+
+
+                viewPersonsagem = new ViewPersonagem();
+                viewPersonsagem.icone = (ImageView) view.findViewById(R.id.icone);
+                viewPersonsagem.titulo = (TextView) view.findViewById(R.id.titulo);
+                viewPersonsagem.descricao = (TextView) view.findViewById(R.id.descricao);
+
+                view.setTag(viewPersonsagem);
+
+                DadosPersonagem dadosPersonagem;
+                dadosPersonagem = (DadosPersonagem) this.getItem(position);
+                viewPersonsagem.icone.setImageResource(dadosPersonagem.getIcone());
+                viewPersonsagem.titulo.setText(dadosPersonagem.getTitulo());
+                viewPersonsagem.descricao.setText(dadosPersonagem.getDescricao());
+            }
+
+
+            return view;
         }
     }
 }
