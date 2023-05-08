c5f265519b50bb99c3f993a187e3510feec0f5c1
Leo Trevisol <leotrevisol.2021@gmail.com>
2023-05-07 23:10:22 -0300

    ajustando classes

--- CardsListView/app/src/main/java/com/example/cardslistview/MainActivity.java
+++ CardsListView/app/src/main/java/com/example/cardslistview/MainActivity.java
@@ -20,14 +20,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String[] listaNomes ={"Naruto", "Jojos", "Haikyuu", "Fullmetal alchemist", "Yuyu Hakusho"};
+    private final String[] listaNomes ={"Naruto", "Jojos Bizare Adventure", "Haikyuu",
+            "Fullmetal Alchemist", "Yuyu Hakusho"};
 
-    private final int[] listaImagens ={R.drawable.cards_foreground, R.drawable.cards_foreground,
-            R.drawable.cards_foreground, R.drawable.cards_foreground, R.drawable.cards_foreground};
+    private final int[] listaImagens ={R.drawable.nruto, R.drawable.jojo,
+            R.drawable.haikyuu, R.drawable.fullmetal, R.drawable.yuyuhakusho,};
 
-    private final String[] listaDescricao ={"Naruto", "Jojos", "Haikyuu", "Fullmetal alchemist", "Yuyu Hakusho"};
-
-    private ListView listView;
+    private final String[] listaDescricao ={"Naruto", "Jojos", "Haikyuu", "Fullmetal", "Yuyu Hakusho"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 //        listView.setAdapter(adapter);
 
 
-        listView = findViewById(R.id.list);
+        ListView listView = findViewById(R.id.list);
 
         ClassAdapter adapter;
         adapter = new ClassAdapter(getApplicationContext(), R.layout.card);
@@ -69,89 +68,6 @@ public class MainActivity extends AppCompatActivity {
         ImageView icone;
         TextView titulo;
         TextView descricao;
-    }
-
-    static class DadosPersonagem{
-        int icone;
-        String titulo;
-        String descricao;
-
-        public DadosPersonagem(int icone, String titulo, String descricao) {
-            this.icone = icone;
-            this.titulo = titulo;
-            this.descricao = descricao;
-        }
-
-        public int getIcone() {
-            return icone;
-        }
-        public void setIcone(int icone) {
-            this.icone = icone;
-        }
-
-        public String getTitulo() {
-            return titulo;
-        }
-
-        public void setTitulo(String titulo) {
-            this.titulo = titulo;
-        }
-
-        public String getDescricao() {
-            return descricao;
-        }
-
-        public void setDescricao(String descricao) {
-            this.descricao = descricao;
-        }
-    }
-
-    static class ClassAdapter extends ArrayAdapter{
-        public ClassAdapter(@NonNull Context context, int resource) {
-            super(context, resource);
-        }
-
-        @Override
-        public void add(@Nullable Object object) {
-            super.add(object);
-        }
-
-        @Override
-        public int getCount() {
-            return super.getCount();
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-            View view;
-            view = convertView;
-            ViewPersonagem viewPersonsagem;
-
-            if(convertView == null){
-                LayoutInflater inflater = (LayoutInflater) this.getContext().
-                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                view = inflater.inflate(R.layout.card,parent,false);
-
-
-                viewPersonsagem = new ViewPersonagem();
-                viewPersonsagem.icone = (ImageView) view.findViewById(R.id.icone);
-                viewPersonsagem.titulo = (TextView) view.findViewById(R.id.titulo);
-                viewPersonsagem.descricao = (TextView) view.findViewById(R.id.descricao);
-
-                view.setTag(viewPersonsagem);
-
-                DadosPersonagem dadosPersonagem;
-                dadosPersonagem = (DadosPersonagem) this.getItem(position);
-                viewPersonsagem.icone.setImageResource(dadosPersonagem.getIcone());
-                viewPersonsagem.titulo.setText(dadosPersonagem.getTitulo());
-                viewPersonsagem.descricao.setText(dadosPersonagem.getDescricao());
-            }
-
-
-            return view;
-        }
     }
 
     public void criarDialog(DadosPersonagem dadosPersonagem){
